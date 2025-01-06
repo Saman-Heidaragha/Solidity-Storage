2aaa24a9b54e41e5fffc0c16478b6ef29cd434fb
Saman <103880448+Saman-Heidaragha@users.noreply.github.com>
2025-01-06 11:26:54 +0300

    Add files via upload

---
+++ SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; // solidity version
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber; 
+
+    //uint256[] listOfFavoriteNumbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function restrieve () public view returns(uint256){
+        return myFavoriteNumber;   
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name) );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+

23e6130befc464a85c43ab36d2764a3775a0dff7
Saman Heidaragha <103880448+Saman-Heidaragha@users.noreply.github.com>
2025-01-15 11:07:12 +0300

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -6,6 +6,7 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
@@ -25,6 +26,13 @@ contract FundMe {
     }
 
     function getConversionRate() public {}
+
+    function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+
+    }
     
     //function withdraw() public {}
 }

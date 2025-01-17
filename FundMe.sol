166f46871e7ec79c6d0e6c0f6eade224a31e20cf
Saman Heidaragha <103880448+Saman-Heidaragha@users.noreply.github.com>
2025-01-17 17:05:42 +0300

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -30,9 +30,14 @@ contract FundMe {
         return uint256(price * 1e10);
     }
 
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        // 1 ETH?
+        
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 
-    function getConversionRate() public {}
 
     function getVersion() public view returns (uint256) {
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();

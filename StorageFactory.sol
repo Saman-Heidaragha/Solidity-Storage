f90654b294bd04cd420a3a15641e770e840e1b5e
Saman Heidaragha <103880448+Saman-Heidaragha@users.noreply.github.com>
2025-01-22 13:49:14 +0300

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,42 +1,27 @@
 // SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
-pragma solidity ^0.8.18;
-
-contract SimpleStorage {
-    uint256 myFavoriteNumber; 
-
-    //uint256[] listOfFavoriteNumbers;
-
-    struct Person{
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    Person[] public listOfPeople;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    }
-
-    function restrieve () public view returns(uint256){
-        return myFavoriteNumber;   
-    }
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push( Person(_favoriteNumber, _name) );
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-}
+import "./SimpleStorage.sol";
 
 contract StorageFactory{
 
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Addree
+        // ABI - Application Binary Interface
+        //SimpleStorage mySimpleStorage = listOfSimpleStorageContracts [_simpleStorageIndex];
+       listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
     }
 
 
+        function sfGet(uint256 _simpleStorageIndex) public view returns(uint) {
+            SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+            return mySimpleStorage.retrieve();
+        }
 }
